5d5fb2227048328f3f22b5d1c97408fe10163be4
armand michael dajab <mdajab@btptek.com>
2024-06-18 00:05:05 +0800

    feat: initial commit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+import "../app/globals.css";
+
+export default function Contact() {
   return (
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>test</h1>
+      Contact
     </main>
-  );
+  )
 }
