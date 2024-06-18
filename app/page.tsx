6b7ea0798910d5bb249f6f0cd79427000e1f1feb
armand michael dajab <mdajab@btptek.com>
2024-06-18 23:41:05 +0800

    chore: added linter and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    
     <main className="min-h-screen">
-        <div className="container mx-auto h-screen bg-white flex items-center justify-center">
-
-        <Image
-      src="/artistify.svg"
-      alt="Artistify.js Logo"
-      width={250}
-      height={60}
-      priority
-    />
-  
-  </div>
+      <div className="container mx-auto flex h-screen items-center justify-center bg-white">
+        <Image src="/artistify.svg" alt="Artistify.js Logo" width={250} height={60} priority />
+      </div>
     </main>
   );
 }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,7 @@
-import "../app/globals.css";
+import '../app/globals.css';
 
 export default function Contact() {
   return (
-
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Contact
-    </main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">Contact</main>
+  );
 }
