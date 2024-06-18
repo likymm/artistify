9f77d4cdc4509aada1e23024169a95a1ef38a3d6
armand michael dajab <mdajab@btptek.com>
2024-06-18 22:54:41 +0800

    feat(lading-page): add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>test</h1>
+    
+    <main className="min-h-screen">
+        <div className="container mx-auto h-screen bg-white flex items-center justify-center">
+
+        <Image
+      src="/artistify.svg"
+      alt="Artistify.js Logo"
+      width={250}
+      height={60}
+      priority
+    />
+  
+  </div>
     </main>
   );
 }
